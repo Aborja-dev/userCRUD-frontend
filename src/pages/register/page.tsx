629cb52ef59324f49e68e8fdfe923be8ad5d6d26
ankahara <hecatombe100@gmail.com>
2024-08-28 01:34:32 -0600

    refactorizar regoster

--- src/pages/register/page.tsx
+++ src/pages/register/page.tsx
@@ -1,12 +1,23 @@
-import RegisterForm from '@/pages/register/components/registerForm'
+import RegisterForm, { RegisterFormData } from '@/pages/register/components/registerForm'
 import { fetchRegister } from '@/service/api_gateway'
+import { Alert } from '@/shared/Alerts'
 import { Button } from '@/shared/Buttons'
+import { useNavigate } from 'react-router-dom'
 
 const RegisterPage = () => {
+  const navigate = useNavigate()
+  const registerHandler = async (values: RegisterFormData) => {
+    const result = await fetchRegister(values)
+    if ('error' in result) {
+      return Alert.error(result.message)
+    }
+    navigate('/login')
+    return Alert.success('Usuario creado')
+  }
   return (
     <div>
       <h1>Crear Nuevo Usuario</h1>
-      <RegisterForm onSubmit={(values) => fetchRegister(values)}>
+      <RegisterForm onSubmit={registerHandler}>
         <Button.Back>Regresar</Button.Back>
       </RegisterForm>
     </div>
